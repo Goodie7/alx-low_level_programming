c5500189a161cbe67115c55bb84763f947629544
Goodie7 <abogoodness8@gmail.com>
2023-03-06 15:13:02 -0800

    my 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,12 +6,9 @@
  * @n: bytes filled.
  * Return: the pointer to dest.
  */
-char *_memset(char *s, char b, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		*(s + i) =  b;
-
-	return (s);
+char *_memset(char *s, char b, unsigned int n) {
+    for (unsigned int i = 0; i < n; i++) {
+        s[i] = b;
+    }
+    return s;
 }
